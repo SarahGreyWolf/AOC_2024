867e74c3a444d7678ebe9ffffe458f3105b3d085
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2024-12-03 10:48:12 +0000

    feat(day03): complete part 2

--- day_3/main.go
+++ day_3/main.go
@@ -12,26 +12,28 @@ func main() {
 	if len(args) != 1 {
 		fmt.Println("Usage: go run main.go <path_to_file.txt>")
 	}
-	bytes, err := os.ReadFile(args[0])
+	input, err := os.ReadFile(args[0])
 
 	if err != nil {
 		panic(err)
 	}
 
 	// text := string(bytes)
-	result := part1(&bytes)
+	result := part1(&input)
 	fmt.Printf("The result of all mul instructions is %d\n", result)
+	result = part2(&input)
+	fmt.Printf("The result of all mul instructions abiding by do and don't is %d\n", result)
 
 }
 
-func part1(bytes *[]byte) int {
+func part1(input *[]byte) int {
 	exp, err := regexp.Compile(`mul\((\d*),(\d*)\)`)
 
 	if err != nil {
 		panic(err)
 	}
 
-	found := exp.FindAllSubmatch(*bytes, -1)
+	found := exp.FindAllSubmatch(*input, -1)
 
 	if len(found) == 0 {
 		panic("Found zero matching")
@@ -47,4 +49,36 @@ func part1(bytes *[]byte) int {
 	return results
 }
 
+func part2(input *[]byte) int {
+	exp, err := regexp.Compile(`(mul\((\d*),(\d*)\)|don't\(\)|do\(\))`)
+
+	if err != nil {
+		panic(err)
+	}
+
+	found := exp.FindAllSubmatch(*input, -1)
+
+	if len(found) == 0 {
+		panic("Found zero matching")
+	}
+
+	results := 0
+	do := true
+	for i := range len(found) {
+		if string(found[i][1]) == "do()" {
+			do = true
+			continue
+		} else if string(found[i][1]) == "don't()" {
+			do = false
+			continue
+		}
+		if !do {
+			continue
+		}
+		lhs, _ := strconv.Atoi(string(found[i][2]))
+		rhs, _ := strconv.Atoi(string(found[i][3]))
+		results += lhs * rhs
+	}
+
+	return results
 }
